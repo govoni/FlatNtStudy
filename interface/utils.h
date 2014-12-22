9f57fd3babeadf28b99e6e21e9b00ab5607fefdb
Raffaele <raffaele.gerosa@cern.ch>
2014-12-22 22:04:51 +0100

    new methods and objects

--- interface/utils.h
+++ interface/utils.h
@@ -1,63 +1,111 @@
 #ifndef utils_h
 #define utils_h
 
-#include "TChain.h"
-#include "TLorentzVector.h"
-#include "readTree.h"
 #include <vector>
 
+#include "TChain.h"
+#include "TLorentzVector.h"
+#include "plotter.h"
+
+#include "readTree.h"
+#include "ReadInputFile.h"
+
+using namespace std ;
+
+// class to contain lepton information
 class leptonContainer {
 
  public:
 
-  leptonContainer(TLorentzVector lepton4V, float charge, float iso):
+  leptonContainer(){};
+  ~leptonContainer(){};
+
+  leptonContainer(TLorentzVector lepton4V, float charge, int flavour, float iso):
    lepton4V_(lepton4V),
    charge_(charge),
+   flavour_(flavour),
    iso_(iso){};  
 
   leptonContainer(TLorentzVector lepton4V, float iso):
    lepton4V_(lepton4V),
    charge_(-999),
+   flavour_(-999),
    iso_(iso){};  
+
+   bool operator < ( const leptonContainer & lepton2){
+     if(lepton4V_.Pt() < lepton2.lepton4V_.Pt()) return true;
+     else return false;
+   };
+
+   bool operator == ( const leptonContainer & lepton2){
+     if(lepton4V_ == lepton2.lepton4V_ and charge_ == lepton2.charge_ ) return true;
+     else return false;
+   };
+
 
    TLorentzVector lepton4V_ ;
    float charge_ ;
+   int   flavour_ ;
    float iso_ ;
 };
 
+// class to contain jet information
+class jetContainer {
+
+ public:
+
+  jetContainer(){};
+  ~jetContainer(){};
+
+
+  jetContainer(TLorentzVector jet4V, float jetPUID, float btag):
+   jet4V_(jet4V),
+   jetPUID_(jetPUID),
+   btag_(btag){};  
+
+   jetContainer(TLorentzVector jet4V):
+   jet4V_(jet4V),
+   jetPUID_(-999),
+   btag_(-999){};  
+
+   bool operator < ( const jetContainer & jet2){
+     if(jet4V_.Pt() < jet2.jet4V_.Pt()) return true;
+     else return false;
+   };
+
+   bool operator == ( const jetContainer & jet2){
+     if(jet4V_ == jet2.jet4V_ and jetPUID_ == jet2.jetPUID_ and btag_ == jet2.btag_ ) return true;
+     else return false;
+   };
+
+
+   TLorentzVector jet4V_ ;
+   float jetPUID_ ;
+   float btag_ ;
+
+};
+
+// delta phi function
 double deltaPhi (double phi1, double phi2) ;
 
-bool closeToLeptons (float eta, float phi, std::vector<TLorentzVector> & TL_leptons, float R = 0.3) ;
+// check if an object is within a dR with another one
+bool closeToLeptons (float eta, float phi, vector<leptonContainer> & TL_leptons, float R = 0.3) ;
 bool closeToLeptons (float eta, float phi, readTree & reader, float R = 0.3) ;
 
-/*
- - translates the 8 sets of variables into 8 sets of arrays
-*/
-void fillRecoJetArray (float * pt, float * eta, float * phi, 
-                       float * mass, float * ID, float * btag, readTree & reader) ;
-void fillPuppiJetArray (float * pt, float * eta, float * phi, 
-                        float * mass, float * ID, float * btag, readTree & reader) ;
-void fillGenJetArray (float * pt, float * eta, float * phi, 
-                      float * mass, float * ID, float * btag, readTree & reader) ;
+// fill Reco Jet Array from the readTree object
+void fillRecoJetArray  (vector<jetContainer> & jetVector, readTree & reader) ;
+void fillPuppiJetArray (vector<jetContainer> & jetVector, readTree & reader) ;
+void fillGenJetArray   (vector<jetContainer> & jetVector, readTree & reader) ;
 
-/*
- - translates the 4 sets of variables into 4 sets of arrays
-*/
-void fillRecoLeptonsArray (float * pt, float * eta, float * phi, float * iso, readTree & reader) ;
-void fillRecoLeptonsArray (float * pt, float * eta, float * phi, float * iso, float* charge, readTree & reader) ;
+void fillRecoLeptonsArray (vector<leptonContainer> & jetVector, readTree & reader) ;
 
 /*
  - copies leptons in the vector
  - removes leptons with the minimum pt threshold 
  - applies the isolation according to isocut
 */
-void dumpLeptons (std::vector<TLorentzVector> & TL_leptons, 
-                  float * pt, float * eta, float * phi, float * iso, float isocut, float minptcut) ;
-void dumpLeptons (std::vector<TLorentzVector> & TL_leptons, 
-                    float * pt, float * eta, float * phi, float * iso, float* charge, float isocut, float minptcut) ;
 
-void dumpLeptons(std::vector<leptonContainer> & TL_leptons, float * pt, float * eta, float * phi, float * iso, float isocut, float minptcut) ;
-void dumpLeptons(std::vector<leptonContainer> & TL_leptons, float * pt, float * eta, float * phi, float * iso, float* charge, float isocut, float minptcut) ;
+vector<leptonContainer> dumpLeptons(std::vector<leptonContainer> & TL_leptons, float isocut, float minptcut) ;
 
 /*
  - copies jets in the vector
@@ -65,25 +113,32 @@ void dumpLeptons(std::vector<leptonContainer> & TL_leptons, float * pt, float * 
    pass the pt threshold lepminptcut
  - applies the btagging
 */
-void dumpJets (std::vector<TLorentzVector> & TL_jets, std::vector<TLorentzVector> & TL_leptons, 
-               float * pt, float * eta, float * phi, float * mass, float * btag, 
-               float minptcut, float btagcut, float lepminptcut, float deltaR, int Njets = 8) ;
 
-void dumpJets (std::vector<TLorentzVector> & TL_jets, std::vector<leptonContainer> & TL_leptons, 
-               float * pt, float * eta, float * phi, float * mass, float * btag, 
-               float minptcut, float btagcut, float lepminptcut, float deltaR, int Njets = 8) ;
+vector<jetContainer> dumpJets (vector<jetContainer> & TL_jets, std::vector<leptonContainer>  & TL_leptons, 
+                               float minptcut, float btagcut, float jetPUIDcut, float lepminptcut, float deltaR) ;
 
 /*
  - translates the 8 sets of variables into 8 sets of arrays
 */
-void fillTrackJetArray (float * pt, float * eta, float * phi, float * mass, readTree & reader) ;
+void fillTrackJetArray (vector<jetContainer> & jetVector, readTree & reader) ;
 /*
  - copies jets in the vector
  - removes jets closer than deltaR to the first two leptons, if the first two leptons 
    pass the pt threshold lepminptcut
 */
-void dumpTrackJets (std::vector<TLorentzVector> & TL_jets, std::vector<TLorentzVector> & TL_leptons, 
-                    float * pt, float * eta, float * phi, float * mass, 
-                    float minptcut, float lepminptcut, float deltaR) ;
+vector<jetContainer> dumpTrackJets (vector<jetContainer> & TL_jets, vector<leptonContainer> & TL_leptons, 
+                                    float minptcut, float lepminptcut, float deltaR) ;
+
+// method to fill histograms                                                                                                                                            
+            
+void fillHistos (plotter & analysisPlots,
+                 readTree* reader,
+                 vector<cutContainer> & CutList,
+                 vector<variableContainer> & VariableList,
+                 const string & sampleName,     const bool & usePuppiAsDefault,
+                 const double & minPtLeptonCut, const double & minPtLeptonCutCleaning,
+                 const double & leptonIsoCut,   const double & leptonIsoLooseCut,
+                 const double & matchingCone,   const double & minJetCutPt,
+                 map<string,TH1F*> & vect);
 
 #endif
