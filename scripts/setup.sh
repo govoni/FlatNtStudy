ff505d32cf158f1aeb4c952ac323239838d5f3b6
Raffaele <raffaele.gerosa@cern.ch>
2015-01-27 14:19:41 +0100

    updates

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,6 +8,8 @@ fi
 export PATH=${PATH}:${THISDIR}/bin
 export FlatNtStudy=${THISDIR}/
 
+export ROOFITSYS=${ROOTSYS}/roofit
+
 source /afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
 
 if [ -f /data2/rgerosa/root/bin/thisroot.sh ] 
