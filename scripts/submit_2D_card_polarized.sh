8701ffa38b24b0e9ea0b370f4ebe451b2b755f5c
Raffaele <raffaele.gerosa@cern.ch>
2015-02-07 16:18:45 +0100

    updates

---
+++ scripts/submit_2D_card_polarized.sh
@@ -0,0 +1,31 @@
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR maximumLikelihoodFit --batchMode --queque cmscaf1nd --channel UU --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR maximumLikelihoodFit --batchMode --queque cmscaf1nd --channel EE --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR maximumLikelihoodFit --batchMode --queque cmscaf1nd --channel DF --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR maximumLikelihoodFit --batchMode --queque cmscaf1nd --channel COMB --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel UU --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel EE --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel DF --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel COMB --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel UU --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel EE --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel DF --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel COMB --nToys 100 --outputTree 1 --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR LikelihoodScan --batchMode --queque cmscaf1nh --channel UU --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR LikelihoodScan --batchMode --queque cmscaf1nh --channel EE --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR LikelihoodScan --batchMode --queque cmscaf1nh --channel DF --rMin -2 --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Polarized/Card2D/ --outputDIR LikelihoodScan --batchMode --queque cmscaf1nh --channel COMB --rMin - --rMax 3 --inputVariableList cfg/DataCardCfg/PolarizedCards/Variables2D.txt --injectSignal 1 --is2Dcard
