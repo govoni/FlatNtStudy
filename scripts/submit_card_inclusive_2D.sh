d871adad86ccc24599fae251e82506e2d1076e7c
Raffaele <raffaele.gerosa@cern.ch>
2015-02-08 09:50:39 +0100

    card to submit jobs

---
+++ scripts/submit_card_inclusive_2D.sh
@@ -0,0 +1,31 @@
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeMaximumLikelihoodFit --batchMode --queque cmscaf1nd --channel UU --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeMaximumLikelihoodFit --batchMode --queque cmscaf1nd --channel EE --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeMaximumLikelihoodFit --batchMode --queque cmscaf1nd --channel DF --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --maximumLikelihoodFit --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeMaximumLikelihoodFit --batchMode --queque cmscaf1nd --channel COMB --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel UU --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel EE --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel DF --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeProfileLikelihood --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeProfileLikelihood --batchMode --queque cmscaf1nh --channel COMB --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel UU --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel EE --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel DF --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --computeAsymptotic --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeAsymptotic --batchMode --queque cmscaf1nd --channel COMB --nToys 100 --outputTree 1 --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeLikelihoodScan --batchMode --queque cmscaf1nh --channel UU --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard --nToys 100 --outputTree 1
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeLikelihoodScan --batchMode --queque cmscaf1nh --channel EE --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard --nToys 100 --outputTree 1
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeLikelihoodScan --batchMode --queque cmscaf1nh --channel DF --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard --nToys 100 --outputTree 1
+
+python python/runCombine.py  --makeLikelihoodScan --datacardDIR output/DataCards_WW_SS_Inclusive/Card2D/ --outputDIR computeLikelihoodScan --batchMode --queque cmscaf1nh --channel COMB --rMin 0.5 --rMax 1.5 --inputVariableList cfg/DataCardCfg/InclusiveCards/Variables2D.txt --injectSignal 1 --is2Dcard --nToys 100 --outputTree 1
