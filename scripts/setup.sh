c84cce1339b0bef197b5465d63aa09be6b6eac33
Raffaele <raffaele.gerosa@cern.ch>
2015-03-10 13:34:06 +0100

    fixed a problem with LD_LYBRARY_PATH

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,22 @@
 export THISDIR=`pwd`
+
+if [ -n "${DYLD_LIBRARY_PATH}" ] ; then
+    unset DYLD_LIBRARY_PATH
+fi
+
+if [ -z "$CMSSW_BASE" ] ; then
+    eval `scramv1 runtime -sh`
+fi
+
+unset LD_LIBRARY_PATH
+unset FlatNtStudy 
+
+if [ -z "$CMSSW_BASE" ] ; then
+    eval `scramv1 runtime -sh`
+else
+    eval `scramv1 runtime -sh`
+fi
+
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:${THISDIR}/lib
 
 if [ -n "${DYLD_LIBRARY_PATH}" ] ; then
@@ -18,4 +36,3 @@ else
     source /afs/cern.ch/sw/lcg/contrib/gcc/4.8.1/x86_64-slc6-gcc48-opt/setup.sh
     source /afs/cern.ch/sw/lcg/app/releases/ROOT/6.03.02/x86_64-slc6-gcc48-opt/root/bin/thisroot.sh
 fi
-
