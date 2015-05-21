818d0c97387c6ec815cf9c7ee2ca9de1b581daec
Raffaele <raffaele.gerosa@cern.ch>
2015-05-21 20:43:34 +0200

    updates

---
+++ scripts/submit_datacardsProduction_HvsNoH_PhaseI.sh
@@ -0,0 +1,17 @@
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_SF/datacardDynamic_WW_SS_SF_UUpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_UUpp_PhaseI -b --cleanJobDir --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_SF/datacardDynamic_WW_SS_SF_UUmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_UUmm_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_SF/datacardDynamic_WW_SS_SF_EEpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_EEpp_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_SF/datacardDynamic_WW_SS_SF_EEmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_EEmm_PhaseI -b --submit
+
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_DF/datacardDynamic_WW_SS_EUpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_EUpp_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_DF/datacardDynamic_WW_SS_EUmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_EUmm_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_DF/datacardDynamic_WW_SS_UEpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_UEpp_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/HvsNoHCards/WW_SS_DF/datacardDynamic_WW_SS_EUmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsHvsNoH --jobName datacardDynamic_EUmm_PhaseI -b --submit
+
---
+++ scripts/submit_datacardsProduction_polarized_PhaseI.sh
@@ -0,0 +1,17 @@
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_SF/datacardDynamic_WW_SS_SF_UUpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_UUpp_PhaseI -b --cleanJobDir --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_SF/datacardDynamic_WW_SS_SF_UUmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_UUmm_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_SF/datacardDynamic_WW_SS_SF_EEpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_EEpp_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_SF/datacardDynamic_WW_SS_SF_EEmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_EEmm_PhaseI -b --submit
+
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_DF/datacardDynamic_WW_SS_EUpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_EUpp_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_DF/datacardDynamic_WW_SS_EUmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_EUmm_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_DF/datacardDynamic_WW_SS_UEpp_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_UEpp_PhaseI -b --submit
+
+python python/runDataCards.py --queque 1nd --configfile cfg/DataCardCfg/PolarizedCards/WW_SS_DF/datacardDynamic_WW_SS_EUmm_PhaseI.cfg --executable bin/MakeDatacardsDynamic_WW.exe --jobDir output/outputJobsPolarized --jobName datacardDynamic_EUmm_PhaseI -b --submit
+
